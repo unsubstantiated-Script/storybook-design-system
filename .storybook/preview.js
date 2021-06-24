b5ee9a1e5de7061b2d714f4e87f2152e73a2a58f
Justin Fulton <jusfulton@gmail.com>
2021-06-24 11:40:11 -0700

    added accessability tests

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,10 +12,11 @@ export const decorators = [
 
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
-	controls: {
-		matchers: {
-			color: /(background|color)$/i,
-			date: /Date$/,
-		},
+	// Storybook a11y addon configuration
+	a11y: {
+		// the target DOM element
+		element: "#root",
+		// sets the execution mode for the addon
+		manual: false,
 	},
 };
